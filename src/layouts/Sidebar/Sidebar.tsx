d355f423bf1e62880e09a81a48bec15faa1a249c
Yuriy <17uraz12@gmail.com>
2022-05-16 16:31:46 +0300

    improve sidebar landing

--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -1,12 +1,18 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import { FaUserFriends } from 'react-icons/fa'
+import { BiNews, BiPhotoAlbum, BiLogOutCircle } from 'react-icons/bi'
+import { CgProfile } from 'react-icons/cg'
+import { BsFillChatDotsFill } from 'react-icons/bs'
 import { AiOutlineArrowRight } from 'react-icons/ai'
 import cn from 'classnames'
 import styles from './Sidebar.module.scss'
 
 interface Props {}
 const Sidebar: FC<Props> = ({}: Props) => {
+	const [isClosed, setIsClosed] = useState<boolean>(false)
+
 	return (
-		<nav className={styles.sidebarWrapper}>
+		<nav className={cn(styles.sidebarWrapper, { [styles.close]: isClosed })}>
 			<header>
 				<div className={styles.imageText}>
 					<span className={styles.image}>
@@ -15,15 +21,50 @@ const Sidebar: FC<Props> = ({}: Props) => {
 							alt='logo'
 						/>
 					</span>
-					<span className={styles.text}>Telegram</span>
+					<span className={cn(styles.text, { [styles.fadeOut]: isClosed })}>
+						Telegram
+					</span>
 				</div>
-				<span className={styles.rightArrowIcon}>
+				<span
+					className={styles.rightArrowIcon}
+					onClick={() => setIsClosed(prevState => !prevState)}
+				>
 					<AiOutlineArrowRight />
 				</span>
 			</header>
 			<div className={styles.menuBar}>
-				<div className={styles.menu}>menu</div>
-				<div className={styles.bottomContent}>bootom</div>
+				<div className={styles.menu}>
+					<ul className={styles.optionList}>
+						<li className={cn(styles.option, { [styles.fadeOut]: isClosed })}>
+							<CgProfile />
+							<span>My Profile</span>
+						</li>
+						<li className={cn(styles.option, { [styles.fadeOut]: isClosed })}>
+							<BiNews />
+							<span>News</span>
+						</li>
+						<li className={cn(styles.option, { [styles.fadeOut]: isClosed })}>
+							<BsFillChatDotsFill />
+							<span>Chat</span>
+						</li>
+						<li className={cn(styles.option, { [styles.fadeOut]: isClosed })}>
+							<FaUserFriends />
+							<span>Friends</span>
+						</li>
+						<li className={cn(styles.option, { [styles.fadeOut]: isClosed })}>
+							<BiPhotoAlbum />
+							<span>Photo</span>
+						</li>
+					</ul>
+				</div>
+				<div className={styles.bottomContent}>
+					<ul className={styles.optionList}>
+						<li className={cn(styles.option, { [styles.fadeOut]: isClosed })}>
+							<BiLogOutCircle />
+							<span>Log Out</span>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	)
